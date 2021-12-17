922218867f52f36b37d5320c5b8dc1056df0fa15
Lymeng <lymengchun@gmail.com>
2021-12-17 13:36:35 +0700

    done database to chat app

--- index.js
+++ index.js
@@ -4,20 +4,41 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const mongoose = require("mongoose");
+const mongoDB = 'mongodb+srv://admin:admin@chat-app-simple.qj7of.mongodb.net/chat-app-simple?retryWrites=true&w=majority';
+const Msg = require('./models/messages');
+
+mongoose.connect(mongoDB,{useNewUrlParser: true,useUnifiedTopology:true}).then(() => {
+  console.log('connected to mongodb');
+}
+).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+
+  Msg.find().then(result=>{
+    socket.emit('output-messages',result )
+  }
+  )
+
+
   console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 
   socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message',msg);
+    // console.log('message: ' + msg);
+
+    const message = new Msg({msg});
+    message.save().then(() => {
+      io.emit('chat message',msg);
+    }
+    )
+    
   });
 
 
